52fe5538fac42e099bb8a48269d1e1ad88827ea1
Yanbo Cheng <86802756+iamyanbo@users.noreply.github.com>
2023-10-06 14:44:43 -0400

    Shelter profile implementation

--- scripts/edit-profile.js
+++ scripts/edit-profile.js
@@ -2,7 +2,7 @@ const profileImageInput = document.getElementById('profile-image');
 const profileImage = document.querySelector('.profile-img img');
 
 profileImageInput.addEventListener('change', (e) => {
-    const selectedImage = e.target.files[0]; // Get the selected file
+    const selectedImage = e.target.files[0];
     if (selectedImage) {
         const reader = new FileReader();
         reader.onload = (event) => {
@@ -13,7 +13,6 @@ profileImageInput.addEventListener('change', (e) => {
 });
 
 const profileForm = document.getElementById('profile-form');
-console.log(profileForm);
 profileForm.addEventListener('submit', function (e) {
     e.preventDefault();
     if (!profileForm.checkValidity()) {
