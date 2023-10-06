0bbab8680b81962a6d0a0915950d2a9428cff377
Jorryn Lu <jorryn.lu@mail.utoronto.ca>
2023-10-06 07:16:08 -0400

    Implement login/signup pages
    Create login and signup forms for the login signup pages.
    
    The login and signup pages only offer a one-way connection with the rest
    of the website (i.e. cannot get to login/signup from the main website)
    for P1 because P1 requirements specify that all views are in a logged in
    perspective.
    
    Include simple js for toggling seeker/shelter tab and displaying the
    error message. These toggles will lead to different landing pages when
    the user clicks login.
    
    Hardcode js to display an error message when the username is "abc".

---
+++ scripts/signup.js
@@ -0,0 +1,37 @@
+// seeker and shelter toggle 
+let seeker = document.querySelector('#seeker');
+let shelter = document.querySelector('#shelter');
+var x = 'seeker';
+
+seeker.addEventListener('click', () => {
+    seeker.classList.add('active');
+    shelter.classList.remove('active');
+    x = 'seeker';
+});
+shelter.addEventListener('click', () => {
+    seeker.classList.remove('active');
+    shelter.classList.add('active');
+    x = 'shelter';
+});
+
+// signup form validation
+// const signupForm = document.getElementById('signup__form');
+const signupForm = document.getElementById('signup__form');
+
+signupForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    const username = document.getElementById("username");
+    
+    if (username.value == 'abc') {
+        const signupError = document.getElementById('signup-error');
+        signupError.classList.add("display");
+        return;
+    }
+    
+    if (!signupForm.checkValidity()) {
+        return;
+    }
+
+    window.location.href = 'signup-success.html';
+})
