dca2e1113a167a37f3132bef886f23eb1bd93ef6
Jorryn Lu <jorryn.lu@mail.utoronto.ca>
2023-10-08 16:05:04 -0400

    Add error message to login page
    Add error message similar to the one on the signup page. When the user
    enters the password as "abc", the page will show an error saying that
    the password or username is invalid.

--- scripts/login.js
+++ scripts/login.js
@@ -22,9 +22,17 @@ loginForm.addEventListener('submit', function (e) {
         return;
     }
 
+    // Check username and password validity
+    const password = document.getElementById("login_pwd");
+    if (password.value == 'abc') {
+        const loginError = document.getElementById('login-error');
+        loginError.classList.add("display");
+        return;
+    }
+
     if (x == 'seeker') {
         window.location.href = 'seeker/home.html';
     } else {
         window.location.href = 'shelter/home.html';
     }
-})
+});
